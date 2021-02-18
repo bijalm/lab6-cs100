f6bf055626a3c8252f3495e0dec067e83b886451
Keeevini <kevin.ni2000@gmail.com>
2021-02-17 20:09:05 -0800

    Added failing tests

---
+++ src/rectangle.cpp
@@ -0,0 +1,15 @@
+#include "../header/rectangle.hpp"
+
+Rectangle::Rectangle(int w, int h){
+	Rectangle::set_width(w);
+	Rectangle::set_height(h);
+
+}
+
+int Rectangle::area() {
+	return -1;
+}
+
+int Rectangle::perimeter() {
+	return -1;
+}
